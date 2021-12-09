272080963c4645fa33190f2d071346cfd16cc0e6
Potatoes <cd64@mail.com>
2021-12-09 08:20:28 -0500

    bump to v3.0.4

--- src/Soup/src/BepInExPlugin.cs
+++ src/Soup/src/BepInExPlugin.cs
@@ -34,4 +34,11 @@ namespace PotatoesSoup
 			//Harmony.CreateAndPatchAll(typeof(QuickerClip));
 		}
 	}
+	
+	internal static class PluginInfo
+	{
+		internal const string NAME = "Potatoes' Soup";
+		internal const string GUID = "dll.potatoes1286.soup";
+		internal const string VERSION = "3.0.4";
+	}
 }
--- src/Soup/src/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace PotatoesSoup
-{
-	internal static class PluginInfo
-	{
-		internal const string NAME = "Potatoes' Soup";
-		internal const string GUID = "dll.potatoes1286.soup";
-		internal const string VERSION = "3.0.3";
-	}
-}
