a5e564d43e3dd7c1341c956502116c865bfae519
Potatoes <cd64@mail.com>
2021-12-10 20:21:29 -0500

    Added extra checks

--- src/Soup/src/EjectorHit.cs
+++ src/Soup/src/EjectorHit.cs
@@ -1,4 +1,5 @@
-﻿using FistVR;
+﻿using System.Linq;
+using FistVR;
 using HarmonyLib;
 using UnityEngine;
 
@@ -10,12 +11,13 @@ namespace PotatoesSoup
 		[HarmonyPostfix]
 		public static void RevolverEjector_FVRUpdate_HitEjector(RevolverEjector __instance)
 		{
-			if (__instance.Magnum.IsHeld)
+			if (__instance.Magnum.IsHeld && !__instance.Magnum.m_isCylinderArmLocked)
 			{
 				FVRViveHand otherHand = __instance.Magnum.m_hand.OtherHand;
+				if (otherHand.CurrentInteractable is Speedloader) if (!(otherHand.CurrentInteractable as Speedloader).Chambers.Any(x => x.IsLoaded)) return;
 				float dist = Vector3.Distance(__instance.transform.position, otherHand.Input.Pos);
-				float dotdist = Vector3.Dot(__instance.transform.forward, otherHand.Input.VelLinearWorld.normalized);
-				if (dist < 0.075f && dotdist > 0.3f)
+				float dotdist = Vector3.Dot(-__instance.transform.forward, otherHand.Input.VelLinearWorld.normalized);
+				if (dist < 0.035f && dotdist > 0.4f)
 				{
 					float magnitude = otherHand.Input.VelLinearWorld.magnitude;
 					if (magnitude > 1f)
