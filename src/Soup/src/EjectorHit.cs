43ba1e243e974623fe222178f35259fbb5d70151
Potatoes <cd64@mail.com>
2021-12-18 22:14:15 -0500

    EjectorHit now accepts palm position

--- src/Soup/src/EjectorHit.cs
+++ src/Soup/src/EjectorHit.cs
@@ -15,9 +15,11 @@ namespace PotatoesSoup
 			{
 				FVRViveHand otherHand = __instance.Magnum.m_hand.OtherHand;
 				if (otherHand.CurrentInteractable is Speedloader) if (!(otherHand.CurrentInteractable as Speedloader).Chambers.Any(x => x.IsLoaded)) return;
+				
 				float dist = Vector3.Distance(__instance.transform.position, otherHand.Input.Pos);
+				float distpalm = Vector3.Distance(__instance.transform.position, otherHand.Input.PalmPos);
 				float dotdist = Vector3.Dot(-__instance.transform.forward, otherHand.Input.VelLinearWorld.normalized);
-				if (dist < 0.035f && dotdist > 0.4f)
+				if ((dist < 0.035f || distpalm < 0.035f) && dotdist > 0.4f)
 				{
 					float magnitude = otherHand.Input.VelLinearWorld.magnitude;
 					if (magnitude > 1f)
@@ -27,6 +29,31 @@ namespace PotatoesSoup
 							__instance.Ejector.localPosition = __instance.RearPos;
 						}
 						__instance.Magnum.EjectChambers();
+					}
+				}
+			}
+		}
+		
+		[HarmonyPatch(typeof(FVRInteractiveObject), "FVRUpdate")]
+		[HarmonyPostfix]
+		public static void SingleActionEjectorRod_FVRUpdate_HitEjector(FVRInteractiveObject __instance)
+		{
+			if (__instance is SingleActionEjectorRod)
+			{
+				SingleActionEjectorRod ejectorrod = __instance as SingleActionEjectorRod;
+				if (ejectorrod.Revolver.IsHeld)
+				{
+					FVRViveHand otherHand = ejectorrod.Revolver.m_hand.OtherHand;
+					float dist = Vector3.Distance(ejectorrod.transform.position, otherHand.Input.Pos);
+					float distpalm = Vector3.Distance(ejectorrod.transform.position, otherHand.Input.PalmPos);
+					float dotdist = Vector3.Dot(-ejectorrod.transform.forward, otherHand.Input.VelLinearWorld.normalized);
+					if ((dist < 0.035f || distpalm < 0.035f) && dotdist > 0.4f)
+					{
+						float magnitude = otherHand.Input.VelLinearWorld.magnitude;
+						if (magnitude > 1f)
+						{
+							ejectorrod.Revolver.EjectPrevCylinder();
+						}
 					}
 				}
 			}
