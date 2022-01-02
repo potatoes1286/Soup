9ac422eca8fffc1cba2b15123d19987483a93622
Potatoes <cd64@mail.com>
2022-01-01 21:05:21 -0500

    BaseGamePatch set; fixed BAWs not supporting suppressors

--- src/Soup/src/BaseGamePatch.cs
+++ src/Soup/src/BaseGamePatch.cs
@@ -1,10 +1,11 @@
-﻿using FistVR;
+﻿using System;
+using FistVR;
 using HarmonyLib;
 using UnityEngine;
 
 namespace PotatoesSoup
 {
-	public class LaserPointerPatch : MonoBehaviour
+	public class BaseGamePatch : MonoBehaviour
 	{
 		[HarmonyPatch(typeof(LaserPointer), "FVRUpdate")]
 		[HarmonyPostfix]
@@ -14,6 +15,14 @@ namespace PotatoesSoup
 			float num = Vector3.Distance(ManagerSingleton<GM>.Instance.m_currentPlayerBody.Head.transform.position, __instance.BeamHitPoint.transform.position);
 			float num2 = Mathf.Lerp(0.01f, 0.2f, num * 0.01f);
 			__instance.BeamHitPoint.transform.localScale = new Vector3(num2, num2, num2);
+		}
+		
+		[HarmonyPatch(typeof(FVRFireArm), "FireMuzzleSmoke", new Type[] { typeof(int) })]
+		[HarmonyPrefix]
+		public static bool FVRFireArm_FireMuzzleSmoke_FixOOB(FVRFireArm __instance, ref int i)
+		{
+			if (__instance.m_muzzleSystems.Count < i) return false;
+			return true;
 		}
 	}
 }
