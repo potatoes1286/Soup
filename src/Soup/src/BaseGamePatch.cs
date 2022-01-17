4c58baeb2f8a79e2d0fa34166511791500021adc
Potatoes <cd64@mail.com>
2022-01-17 13:18:01 -0500

    Corrected Madsen LAR and Panzerschreck sizes

--- src/Soup/src/BaseGamePatch.cs
+++ src/Soup/src/BaseGamePatch.cs
@@ -12,16 +12,32 @@ namespace PotatoesSoup
 		public static void LaserPointerPatch_Update_FixScale(LaserPointer __instance)
 		{
 			//get dist between player head and laser
-			float num = Vector3.Distance(ManagerSingleton<GM>.Instance.m_currentPlayerBody.Head.transform.position, __instance.BeamHitPoint.transform.position);
+			float num = Vector3.Distance(ManagerSingleton<GM>.Instance.m_currentPlayerBody.Head.transform.position,
+										 __instance.BeamHitPoint.transform.position);
 			float num2 = Mathf.Lerp(0.01f, 0.2f, num * 0.01f);
 			__instance.BeamHitPoint.transform.localScale = new Vector3(num2, num2, num2);
 		}
-		
-		[HarmonyPatch(typeof(FVRFireArm), "FireMuzzleSmoke", new Type[] { typeof(int) })]
+
+		[HarmonyPatch(typeof(FVRFireArm), "FireMuzzleSmoke", new Type[] {typeof(int)})]
 		[HarmonyPrefix]
 		public static bool FVRFireArm_FireMuzzleSmoke_FixOOB(FVRFireArm __instance, ref int i)
 		{
 			if (__instance.m_muzzleSystems.Count < i) return false;
+			return true;
+		}
+
+		[HarmonyPatch(typeof(FVRPhysicalObject), "Awake")]
+		[HarmonyPrefix]
+		public static bool FVRFireArm_Awake_FixScaling(FVRPhysicalObject __instance)
+		{
+			if (__instance.ObjectWrapper == null) return true;
+			if (__instance.ObjectWrapper.ItemID == "MadsenLAR" ||
+				__instance.ObjectWrapper.ItemID == "MagazineMadsenLAR30rnd" ||
+				__instance.ObjectWrapper.ItemID == "MagazineMadsenLAR20rnd" ||
+				__instance.ObjectWrapper.ItemID == "MagazineMadsenLAR10rnd")
+				__instance.transform.localScale = new Vector3(0.88f, 0.88f, 0.88f);
+			if (__instance.ObjectWrapper.ItemID == "PanzerSchreck54")
+				__instance.transform.localScale = new Vector3(0.95f, 0.95f, 1f);
 			return true;
 		}
 	}
