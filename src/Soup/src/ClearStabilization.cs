d594392452faba38b203749df96f9379a7abca8e
Potatoes <cd64@mail.com>
2022-01-17 13:18:43 -0500

    Misc changes to ClearStab

--- src/Soup/src/ClearStabilization.cs
+++ src/Soup/src/ClearStabilization.cs
@@ -10,28 +10,27 @@ namespace PotatoesSoup
 		[HarmonyPrefix]
 		public static bool ClearStabilization_Patch(FVRFireArm __instance)
 		{
-			if (!BepInExPlugin.EnableClearStabilization.Value ||
-			    !GM.Options.QuickbeltOptions.HideControllerGeoWhenObjectHeld)
-			{
-				return true;
-			}
+			if (!BepInExPlugin.EnableClearStabilization.Value || !GM.Options.QuickbeltOptions.HideControllerGeoWhenObjectHeld) return true;
+			
+			//i don't actually understand this if statement.
 			if (__instance.m_hand != null && __instance.m_hand.OtherHand != null && (__instance.m_hand.OtherHand.CurrentInteractable == null || __instance.m_hand.OtherHand.CurrentInteractable is Flashlight || __instance.m_hand.OtherHand.CurrentInteractable is FVRFireArmMagazine))
 			{
 				float num = Vector3.Distance(__instance.m_hand.PalmTransform.position, __instance.m_hand.OtherHand.PalmTransform.position);
 				if (num < BepInExPlugin.ClearStabilizationThreshhold.Value)
-				{
 					__instance.m_hand.OtherHand.Display_Controller.SetActive(false);
-				}
-				else
-				{
-					if (num < BepInExPlugin.ClearStabilizationThreshhold.Value + 0.06f)
-					{
-						ReDisplayViveHand(ref __instance.m_hand.OtherHand);
-					}
-				}
+				else if (num < BepInExPlugin.ClearStabilizationThreshhold.Value + 0.06f)
+					ReDisplayViveHand(ref __instance.m_hand.OtherHand);
 			}
 			return true;
 		}
+		
+		/*[HarmonyPatch(typeof(FVRViveHand), "CurrentInteractable_set")]
+		[HarmonyPrefix]
+		public static bool ClearStabilization_ResetOtherHand_Patch(FVRViveHand __instance)
+		{
+			ReDisplayViveHand(ref __instance.OtherHand);
+			return true;
+		}*/
 
 
 		public static void ReDisplayViveHand(ref FVRViveHand hand)
