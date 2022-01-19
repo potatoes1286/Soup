05f0345a4d9eee9bac8dead840cb772ee25ecf0c
Potatoes <cd64@mail.com>
2022-01-18 19:27:40 -0500

    bump to v4.0.1 (actually last commit, but i needed to write this commit)

--- src/Soup/src/BepInExPlugin.cs
+++ src/Soup/src/BepInExPlugin.cs
@@ -80,6 +80,6 @@ namespace PotatoesSoup
 	{
 		internal const string NAME = "Potatoes' Soup";
 		internal const string GUID = "dll.potatoes1286.soup";
-		internal const string VERSION = "4.0.1";
+		internal const string VERSION = "4.0.1"; 
 	}
 }
