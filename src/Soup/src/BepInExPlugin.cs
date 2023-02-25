4822008d27876ac77c29f7dcd4623ae7ee97982d
Potatoes <48143760+potatoes1286@users.noreply.github.com>
2023-02-25 16:40:04 -0500

    turns out i forgot to bump to 6.0.1 last time. bump to 6.0.2

--- src/Soup/src/BepInExPlugin.cs
+++ src/Soup/src/BepInExPlugin.cs
@@ -111,6 +111,6 @@ namespace PotatoesSoup
 	{
 		internal const string NAME = "Potatoes' Soup";
 		internal const string GUID = "dll.potatoes1286.soup";
-		internal const string VERSION = "6.0.1"; 
+		internal const string VERSION = "6.0.2"; 
 	}
 }
