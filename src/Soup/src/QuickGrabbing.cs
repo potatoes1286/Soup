8f47f1be4fa4179e66d2d91833b7755757667f37
Potatoes <48143760+potatoes1286@users.noreply.github.com>
2024-05-20 20:18:02 -0400

    Add bullets, foregrips to quickgrabbing

--- src/Soup/src/QuickGrabbing.cs
+++ src/Soup/src/QuickGrabbing.cs
@@ -21,51 +21,21 @@ namespace PotatoesSoup
 			    (__instance is OpenBoltChargingHandle		&& BepInExPlugin.QuickGrabbing_GrabBolts.Value) ||
 			    (__instance is BoltActionRifle_Handle		&& BepInExPlugin.QuickGrabbing_GrabBolts.Value) ||
 			    (__instance is TubeFedShotgunBolt			&& BepInExPlugin.QuickGrabbing_GrabBolts.Value) ||
-			    (__instance is FVRFireArmMagazine			&& BepInExPlugin.QuickGrabbing_GrabMags.Value) ||
-			    (__instance is Speedloader					&& BepInExPlugin.QuickGrabbing_GrabMags.Value) ||
+			    (__instance is FVRFireArmMagazine			&& BepInExPlugin.QuickGrabbing_GrabMags.Value)  ||
+			    (__instance is Speedloader					&& BepInExPlugin.QuickGrabbing_GrabMags.Value)  ||
 				(__instance is HandgunSlide					&& BepInExPlugin.QuickGrabbing_GrabPistolSlides.Value) ||
 			    (__instance is FVRHandGrabPoint				&& BepInExPlugin.QuickGrabbing_GrabRopes.Value) ||
 			    (__instance is PinnedGrenade				&& BepInExPlugin.QuickGrabbing_GrabGrenade.Value) ||
 			    (__instance is FVRCappedGrenade				&& BepInExPlugin.QuickGrabbing_GrabGrenade.Value) ||
-			    (__instance is SosigWeaponPlayerInterface	&& BepInExPlugin.QuickGrabbing_GrabSosigWeapon.Value)	 ||
+			    (__instance is SosigWeaponPlayerInterface	&& BepInExPlugin.QuickGrabbing_GrabSosigWeapon.Value) ||
 				(__instance is TubeFedShotgunHandle			&& BepInExPlugin.QuickGrabbing_GrabFores.Value) ||
 				(__instance is FVRAlternateGrip				&& BepInExPlugin.QuickGrabbing_GrabFores.Value) ||
 				(__instance is FVRFireArmGrip				&& BepInExPlugin.QuickGrabbing_GrabFores.Value) ||
 				(__instance is RPG7Foregrip					&& BepInExPlugin.QuickGrabbing_GrabFores.Value) ||
-			 //(__instance is AttachableForegrip			&& BepInExPlugin.QuickGrabbing_GrabFores.Value) ||
-			   (__instance is FVRFireArmTopCover			&& BepInExPlugin.QuickGrabbing_GrabFores.Value)) {
-				
-				//when you grab fvraltgrip it redirects the hand to PrimaryObject
-				//So fvraltgrip is never actually held
-				//and quickgrab thinks that you can grab it when you shouldnt be able to
-				//this fixes that, by checking the primary object's isheld instead of fvraltgrip's
-				if (__instance is FVRAlternateGrip grip)
-					if (grip.PrimaryObject.IsAltHeld)
-						return;
-				if(__instance is FVRFireArmMagazine mag)
-					if (mag.State == FVRFireArmMagazine.MagazineState.Locked)
-						return;
-
-					//ensure not running to prevent accidental grabbing
-				if (BepInExPlugin.QuickGrabbing_DisableWhenRunning.Value && IsArmSwinging(hand) && __instance is not FVRHandGrabPoint) return;
-				//ensure other hand is not the same item
-				if (__instance == hand.OtherHand.CurrentInteractable) return;
-				//ensure bolt action rifle's bolt is not blocked
-				if (__instance is BoltActionRifle_Handle && ((__instance as BoltActionRifle_Handle)!).Rifle.CanBoltMove() == false) return;
-				if (hand.Input.IsGrabbing && hand.m_state == FVRViveHand.HandState.Empty)
-				{
-					if (__instance is SosigWeaponPlayerInterface)
-						if ((__instance as SosigWeaponPlayerInterface)!.W.Type != SosigWeapon.SosigWeaponType.Grenade) return;
-					
-					if (BepInExPlugin.QuickGrabbing_RegrabBolt.Value && __instance is BoltActionRifle_Handle) { //doesnt feel good
-						var bolt = __instance as BoltActionRifle_Handle;
-						bolt.m_wasTPInitiated = true;
-					}
-					
-					hand.Buzz(hand.Buzzer.Buzz_BeginInteraction);
-					hand.ForceSetInteractable(__instance);
-					__instance.BeginInteraction(hand);
-				}
+				(__instance is AttachableForegrip			&& BepInExPlugin.QuickGrabbing_GrabFores.Value) ||
+			    (__instance is FVRFireArmTopCover			&& BepInExPlugin.QuickGrabbing_GrabFores.Value) ||
+			    (__instance is FVRFireArmRound				&& BepInExPlugin.QuickGrabbing_GrabBullets.Value)) {
+					QuickGrab(__instance, hand);
 			}
 		}
 
