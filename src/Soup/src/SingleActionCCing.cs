4a8dee73b4f6a38bc7ab7eb50c61d466b058324c
Potatoes <48143760+potatoes1286@users.noreply.github.com>
2025-02-16 17:52:39 -0500

    Add CCW rotate to streamlined (odd)

--- src/Soup/src/SingleActionCCing.cs
+++ src/Soup/src/SingleActionCCing.cs
@@ -10,12 +10,21 @@ namespace PotatoesSoup
 		[HarmonyPrefix]
 		public static bool SingleActionRevolver_UpdateInteraction_EnableCCing(SingleActionRevolver __instance, ref FVRViveHand hand)
 		{
-			//streamlined users BTFO'd
-			if (!__instance.IsAltHeld && __instance.m_isStateToggled && !__instance.IsAltHeld && !hand.IsInStreamlinedMode && hand.Input.TouchpadDown &&
-				Vector2.Angle(hand.Input.TouchpadAxes, Vector2.up) < 45f)
+			if (!hand.IsInStreamlinedMode && !__instance.IsAltHeld && __instance.m_isStateToggled
+				&& hand.Input.TouchpadDown && Vector2.Angle(hand.Input.TouchpadAxes, Vector2.up) < 45f)
 			{
 				__instance.CurChamber = __instance.PrevChamber;
 				__instance.PlayAudioEvent(FirearmAudioEventType.FireSelector);
+			}
+
+			// So this activates the default CW action
+			// So we do CCW twice!
+			// And don't play the sound bc CW already does it
+			// GENIUS!
+			if (hand.IsInStreamlinedMode && hand.Input.AXButtonDown && hand.Input.BYButtonPressed
+				&& !__instance.IsAltHeld && __instance.m_isStateToggled) {
+				__instance.CurChamber = __instance.PrevChamber;
+				__instance.CurChamber = __instance.PrevChamber;
 			}
 			return true;
 		}
